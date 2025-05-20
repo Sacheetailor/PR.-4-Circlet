7d9808052cb6c27ec3ec05784e1619acbc8c15af
Sacheetailor <sacheetailor@gmail.com>
2025-05-20 11:23:21 +0530

    Add files via upload

---
+++ qus4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k;
+    k=11;
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=1;j<=i;j++)
+        
+        {
+            printf("%d ",k);
+            k++; 
+        }
+        printf("\n");
+    }
+    return 0;
+}
